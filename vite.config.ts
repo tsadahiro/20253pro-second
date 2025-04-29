8ea936629659c2aa131b90262a55e8de99c1a6a1
tsadahiro <sadahiro@tsuda.ac.jp>
2025-04-29 21:16:01 +0900

    second

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/3pro2025-second/', // ★ GitHubリポジトリ名に置き換えてください
+  build: {
+    outDir: 'docs', // 出力先を 'docs' に変更
+  },
 })
